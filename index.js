e7767de23446512833b5b68aabc49f436c04ae6a
ahmed <nemo_ahmed@hotmail.com>
2025-03-31 03:53:31 +0100

    playing around

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const yourOrderPlease_1 = require("./codeWars/yourOrderPlease");
-console.log((0, yourOrderPlease_1.yourOrderPlease)('is2 Thi1s T4est 3a'), 'Thi1s is2 3a T4est');
-console.log((0, yourOrderPlease_1.yourOrderPlease)('4of Fo1r pe6ople g3ood th5e the2'), 'Fo1r the2 g3ood 4of th5e pe6ople');
-console.log((0, yourOrderPlease_1.yourOrderPlease)(''), '');
+const isInteresting_1 = require("./codeWars/isInteresting");
+(0, isInteresting_1.runFn)();
--- index.ts
+++ index.ts
@@ -1,8 +1,2 @@
-import {yourOrderPlease} from './codeWars/yourOrderPlease';
-
-console.log(yourOrderPlease('is2 Thi1s T4est 3a'), 'Thi1s is2 3a T4est');
-console.log(
-  yourOrderPlease('4of Fo1r pe6ople g3ood th5e the2'),
-  'Fo1r the2 g3ood 4of th5e pe6ople',
-);
-console.log(yourOrderPlease(''), '');
+import {runFn} from './codeWars/isInteresting';
+runFn();
